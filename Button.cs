ab5083b0c7d4cfd60996335bdc20071fb0315c7a
Justin Hays <jhays478@gmail.com>
2023-05-29 18:43:26 -0500

    example of GUI and unhandled clicks

---
+++ Button.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+public class Button : Godot.Button
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        this.Connect("pressed", this, "OnPressed");
+    }
+
+    private void OnPressed()
+    {
+        GD.Print("Button pressed.");
+    }
+
+//  // Called every frame. 'delta' is the elapsed time since the previous frame.
+//  public override void _Process(float delta)
+//  {
+//      
+//  }
+}
